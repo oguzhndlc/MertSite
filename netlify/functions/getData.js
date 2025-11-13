6d1001b35aa4c35b55a509bd9c93f87ab73e337c
oguzhndlc <oguzdalg@gmail.com>
2025-11-13 20:38:49 +0300

    Update

---
+++ netlify/functions/getData.js
@@ -0,0 +1,27 @@
+// netlify/functions/get-users.js
+import pkg from "pg";
+const { Client } = pkg;
+
+export async function handler(event, context) {
+  const client = new Client({
+    connectionString: process.env.NETLIFY_DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // Neon için gerekli
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query("SELECT * FROM cards WHERE id = 1;");
+    await client.end();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.rows),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+}
