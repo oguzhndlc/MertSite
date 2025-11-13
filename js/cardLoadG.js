d4177a7dcd8626a94002418c9f5f2ef77b47e6a5
oguzhndlc <oguzdalg@gmail.com>
2025-11-13 22:18:45 +0300

    update

---
+++ js/cardLoadG.js
@@ -0,0 +1,33 @@
+async function loadCards() {
+      try {
+        const response = await fetch('/.netlify/functions/getData'); // Function URL
+        if (!response.ok) throw new Error('Veri çekilemedi');
+
+        const cards = await response.json();
+        const container = document.getElementById('icerikAlani');
+
+        cards.forEach(card => {
+          // Card elementini oluştur
+          const col = document.createElement('div');
+          col.className = 'col-md-4 mb-4';
+
+          col.innerHTML = `
+
+            <div class="card" style="width: 17rem; height: 24rem;">
+                <img src="${card.photo_url}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${card.title}</h5>
+                    <p class="card-text">${card.example}</p>
+                    <a href="Content.html" class="btn btn-primary" id="galery-card-1-btn"></a>
+                </div>
+            </div>
+          `;
+
+          container.appendChild(col);
+        });
+      } catch (error) {
+        console.error('Error fetching cards:', error);
+      }
+    }
+
+    loadCards();
